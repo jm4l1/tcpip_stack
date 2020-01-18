491ee6d63209e9f38db716ea75f2da082a488e98
Jamal Moore <j_mali26@hotmail.com>
2020-01-17 20:01:51 -0400

    - added packet tranmission

--- communications.c
+++ communications.c
@@ -6,8 +6,13 @@
 #include <stdio.h>
 #include <pthread.h>
 #include <string.h>
+#include <sys/utsname.h>
+#include <errno.h>
+#include <netdb.h> 
+
 
 static char recv_buffer[MAX_PACKET_BUFFER_SIZE];
+static char send_buffer[MAX_PACKET_BUFFER_SIZE];
 
 static unsigned int udp_port_number = 40000;
 
@@ -105,4 +110,48 @@ network_start_pkt_receiver_thread(graph_t* topo){
     pthread_create(&recv_pkt_thread , &attr , _network_start_pkt_receiver_thread , (void*) topo);
 
     printf("network receiver thread started for topology \"%s\"\n", topo->topolgy_name);
+}
+
+static int
+_send_pkt_out(int sock_fd , char *pkt_data , unsigned int pkt_size , unsigned int dst_udp_port_no){
+    int rc;
+    struct sockaddr_in dest_addr;
+
+    struct hostent *host = (struct hostent *) gethostbyname("127.0.0.1");
+    dest_addr.sin_family = AF_INET;
+    dest_addr.sin_port = dst_udp_port_no;
+    dest_addr.sin_addr = *((struct in_addr *)host->h_addr);
+
+    rc = sendto(sock_fd,pkt_data,pkt_size,0,(struct sockaddr*)&dest_addr , sizeof(struct sockaddr));
+}
+int
+send_pkt_out(char* pkt , unsigned int pkt_size , interface_t *intf){
+
+    int rc = 0;
+    node_t* sending_node = intf->att_node;
+    node_t* nbr_node = get_nbr_node(intf);
+
+    if(!nbr_node) return -1;
+
+    unsigned int dst_udp_port_no = nbr_node->udp_port_number;
+
+    int sock = socket(AF_INET , SOCK_DGRAM , IPPROTO_UDP);
+
+    if(sock < 0 ){
+        printf("Error : Sending socker created failed , errno = %d" , errno);
+    }
+
+    interface_t* remote_intf  = &intf->link->intf1 == intf ? &intf->link->intf2 : &intf->link->intf1;
+
+    memset(send_buffer , 0 , MAX_PACKET_BUFFER_SIZE);
+
+    char* pkt_wth_aux_data = send_buffer;
+    strcpy(pkt_wth_aux_data , remote_intf->if_name );
+    pkt_wth_aux_data[IF_NAME_SIZE] = '\0';
+    memcpy(pkt_wth_aux_data + IF_NAME_SIZE , pkt , pkt_size);
+
+    rc = _send_pkt_out(sock ,pkt_wth_aux_data , pkt_size + IF_NAME_SIZE ,dst_udp_port_no);
+    close(sock);
+    return rc;
+
 }
--- communications.h
+++ communications.h
@@ -1,1 +1,8 @@
+#ifndef __COMMUNICATIONS_H__
+#define __COMMUNICATIONS_H__
+
 #define MAX_PACKET_BUFFER_SIZE 2048
+
+
+int send_pkt_out(char* pkt , unsigned int pkt_size , interface_t *intf);
+#endif
